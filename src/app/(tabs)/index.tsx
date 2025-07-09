14a423e124df635e05ef182e75e2bd0fe0d2e250
Abayo <eabayo@konza.ke>
2025-07-09 19:10:45 +0300

    types

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
-import Button from '@/components/Button';
+import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View className='flex-1 p-4'>
       <Text className='text-3xl font-bold text-white p-2'>Hello world</Text>
-      <Button />
     </View>
   );
 }
