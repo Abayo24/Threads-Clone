c4d06f71b9a713f978d00e7da408ca8fc171c481
Abayo <eabayo@konza.ke>
2025-07-09 15:28:02 +0300

    navigation dark theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,19 @@
 import { Slot, Stack } from 'expo-router';
 
+import { ThemeProvider, DarkTheme } from '@react-navigation/native';
+
+const myTheme = {
+    ...DarkTheme,
+    colors: {
+        ...DarkTheme.colors,
+        primary: 'white',
+    },
+};
+
 export default function RootLayout() {
     
-    
-    return <Slot />;
+    return (
+    <ThemeProvider value={myTheme}>
+        <Slot />
+    </ThemeProvider>);
 }
