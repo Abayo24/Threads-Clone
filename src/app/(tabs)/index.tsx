8d922263cc3735ce1c0baea4009aa48225e59711
Abayo <eabayo@konza.ke>
2025-07-09 19:00:14 +0300

    path aliases

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Button from '@/components/Button';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text className='text-3xl font-bold text-red-500 p-2'>Feed</Text>
+    <View className='flex-1 p-4'>
+      <Text className='text-3xl font-bold text-white p-2'>Hello world</Text>
+      <Button />
     </View>
   );
 }
